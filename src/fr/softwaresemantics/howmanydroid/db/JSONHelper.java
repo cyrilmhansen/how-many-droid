e61cf9797d5857dec8f05413f5da347229fa69fa
cgoessen <christophe.goessen@gmail.com>
2014-02-03 21:56:18 +0100

    adding JSON import/export sample code

---
+++ src/fr/softwaresemantics/howmanydroid/db/JSONHelper.java
@@ -0,0 +1,54 @@
+package fr.softwaresemantics.howmanydroid.db;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by christophe goessen on 02/02/14.
+ */
+public class JSONHelper {
+    public static void main(String[] args) {
+
+        I18n hello = new I18n();
+        Translation trans = new Translation();
+
+        Locale english = new Locale();
+        Locale fromJSON = new Locale();
+        english.setDescription("english UK");
+        english.setName("en_GB");
+
+
+        Locale french = new Locale();
+        french.setDescription("Français France");
+        french.setName("fr_FR");
+
+        trans.setLocale(english);
+        trans.setValue("Hello World");
+        trans.setI18n(hello);
+        hello.setTranslations(new ArrayList<Translation>());
+        hello.getTranslations().add(trans);
+        hello.setMsgID("HELLO_WORLD");
+        trans = new Translation();
+        trans.setLocale(french);
+        trans.setValue("Bonjour Monde");
+        trans.setI18n(hello);
+        hello.getTranslations().add(trans);
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            System.out.println(mapper.writeValueAsString(english));
+            fromJSON = mapper.readValue(mapper.writeValueAsString(english), Locale.class);
+            System.out.println(fromJSON.getDescription());
+
+            System.out.println(mapper.writeValueAsString(hello));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
